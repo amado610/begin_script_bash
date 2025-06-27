95498ab473cf5aa0f4fc272763da2d53839152c0
amado610 <goncalocarreiroamado@gmail.com>
2025-06-27 18:13:26 +0000

    while loops

---
+++ while_lops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#myvar=1
+
+#while [ $myvar -le 10 ]
+#do
+#	echo $myvar
+#	myvar=$(( $myvar + 1 ))
+#	sleep 0.5
+#done
+
+#-------------------------------
+
+
+while [ -f ~/testfile ]
+do
+	echo "As of $(date), the test file exist."
+	sleep 5
+done
+
+echo "As of $(date), the test file has gone missing."
+
