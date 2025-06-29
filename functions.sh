de0d95302aa316bf4fccc751bc510097627efd19
amado610 <goncalocarreiroamado@gmail.com>
2025-06-29 18:03:42 +0000

    functions

---
+++ functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#script to detect de os and execute the rigth update command
+
+release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
+
+#function
+check_exit_status() {
+	if [ $? -ne 0 ]
+	then
+		echo "An error ocurred, please check the $errorlog file."
+	fi
+}
+
+if grep -q "Arch" $release_file
+then
+        #the host is based on arch, run the pacman update command
+        sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	check_exit_status
+fi
+
+if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
+then
+        #the host is based on ubuntu,
+        #run the apt version of the command
+        sudo apt update 1>>$logfile 2>>$errorlog
+        check_exit_status
+
+        sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
+        check_exit_status
+fi
