4058f09b78f7c5bfbd58790f4aeed386d714c437
amado610 <goncalocarreiroamado@gmail.com>
2025-06-29 15:37:49 +0000

    for loops

---
+++ for_loops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#for n in {1..10}
+#do
+#	echo $n
+#	sleep 1
+#done
+
+#echo "This is outside of the for loop."
+
+
+for file in logfiles/*.log
+do
+	tar -czvf $file.tar.gz $file
+done
