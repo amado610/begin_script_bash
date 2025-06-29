b0f14864db1f2e36f27f94f1c59cac235ebe41a8
amado610 <goncalocarreiroamado@gmail.com>
2025-06-29 17:54:25 +0000

    data streams

--- universal_upt_script.sh
+++ universal_upt_script.sh
@@ -3,18 +3,33 @@
 #script to detect de os and execute the rigth update command
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
 then
 	#the host is based on arch, run the pacman update command
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error ocurred, please check the $errorlog file."
+	fi
 fi
 
 if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
 then
 	#the host is based on ubuntu,
 	#run the apt version of the command
-	sudo apt update
-	sudo apt dist-upgrade
+	sudo apt update 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+                echo "An error ocurred, please check the $errorlog file."
+        fi
+
+	sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+        then
+                echo "An error ocurred, please check the $errorlog file."
+        fi
 fi
 
